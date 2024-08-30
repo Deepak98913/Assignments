bde6049e2f9580df8557e996fd5e24a0b1cf4fd1
Deepak Kumar <153820799+Deepak98913@users.noreply.github.com>
2024-08-30 20:43:43 +0530

    Add files via upload

---
+++ Assignment /constants.py/constants.py
@@ -0,0 +1,7 @@
+# constants.py
+
+# Mathematical constant for Pi
+PI = 3.141592653589793
+
+# Speed of light in meters per second
+SPEED_OF_LIGHT = 299792458  # m/s
---
+++ Assignment /constants.py/main.py
@@ -0,0 +1,11 @@
+# main.py
+
+# Import constants from constants.py
+from constants import PI, SPEED_OF_LIGHT
+
+def display_constants():
+    print(f"Value of Pi: {PI}")
+    print(f"Speed of Light: {SPEED_OF_LIGHT} meters per second")
+
+# Call the function to display constants
+display_constants()
